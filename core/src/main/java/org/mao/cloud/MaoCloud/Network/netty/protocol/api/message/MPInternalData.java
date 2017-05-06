6109cd9c9e665f72d0ef70273efb2086b749b2c8
Mao Jianwei <maojianwei2020@gmail.com>
2017-05-06 21:10:05 +0800

    disable cluster internal message

--- core/src/main/java/org/mao/cloud/MaoCloud/Network/netty/protocol/api/message/MPInternalData.java
+++ core/src/main/java/org/mao/cloud/MaoCloud/Network/netty/protocol/api/message/MPInternalData.java
@@ -7,19 +7,19 @@ import org.mao.cloud.MaoCloud.Network.netty.protocol.api.base.MPMessage;
  */
 public interface MPInternalData extends MPMessage {
 
-    // Reader reader();
-    interface Reader extends MPMessage.Reader<MPInternalData>{
-    }
-
-    Writer writer();
-    interface Writer extends MPMessage.Writer{
-    }
-
-
-    Builder builder();
-    interface Builder extends MPMessage.Builder {
-        Builder setNodeName(String name);
-        Builder setNodePassword(String password);
-        MPInternalData build();
-    }
+//    // Reader reader();
+//    interface Reader extends MPMessage.Reader<MPInternalData>{
+//    }
+//
+//    Writer writer();
+//    interface Writer extends MPMessage.Writer{
+//    }
+//
+//
+//    Builder builder();
+//    interface Builder extends MPMessage.Builder {
+//        Builder setNodeName(String name);
+//        Builder setNodePassword(String password);
+//        MPInternalData build();
+//    }
 }
